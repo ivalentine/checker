fb3d7cfdd09735d6e0e4a539f3da4e5b51068f7c
ivalentine <ian@ivalentine.net>
2016-07-22 11:29:43 -0400

    change signup path, submit button label

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :path => '', :path_names => { :sign_up => 'join' }
   resources :lists
   resources :users
   resources :tasks
