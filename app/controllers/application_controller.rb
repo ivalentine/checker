db3895b9ae853fed9dcb92bef2adf19e02a49bda
ivalentine <ian@ivalentine.net>
2016-07-26 15:44:26 -0400

    add SSL challenge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def letsencrypt
+    render text: 'ODoC3TRbenl-bRLw0gFwdqJy6NwWmxG7nTxog8gh-CQ.KBCxhm07gJJ40aNejiEpGkOD4ZBOGGeCf34xYuDMbMg'
+  end
+  
 end
