adede0ceeada9ac2144aafc7e8e0decf5047cddc
ivalentine <ian@ivalentine.net>
2016-08-17 16:22:19 -0400

    add and require first name + last name attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,16 @@ class User < ActiveRecord::Base
   has_many :lists
   has_many :tasks, through: :lists
 
+  validates_presence_of :first_name, :last_name
+
+  # email, password, and password_confirmation are validated by Devise
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable
+
+  def name
+    first_name + " " + last_name
+  end
+
 end
