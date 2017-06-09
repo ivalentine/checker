988f9c151119d5461c02b4b923accd1de0018b29
ivalentine <ian@ivalentine.net>
2017-06-09 12:08:58 -0400

    remove letsencrypt-rails plugin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  mount LetsencryptPlugin::Engine, at: '/'
   devise_for :users, :path => '', :path_names => { :sign_up => 'join' }
   resources :lists
   resources :users
